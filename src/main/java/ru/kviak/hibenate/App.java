e9d326bd15149c8c1b8e04d762090019211cfbdb
Denis Lapin <kviakit@Gmail.com>
2023-08-11 14:19:22 +0300

    Initial commit

---
+++ src/main/java/ru/kviak/hibenate/App.java
@@ -0,0 +1,33 @@
+package ru.kviak.hibenate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import ru.kviak.hibenate.model.Person;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+        Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
+
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+
+        try {
+            session.beginTransaction();
+
+            Person person = session.get(Person.class, 1);
+            System.out.println(person.getName());
+            System.out.println(person.getAge());
+
+            session.getTransaction().commit();
+        }
+       finally {
+            sessionFactory.close();
+        }
+    }
+}
